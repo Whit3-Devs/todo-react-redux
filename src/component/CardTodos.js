8169704d71d69f7c48772e4fb592fb73a6813317
Augusto Blanco <blanco.augusto.r@gmail.com>
2022-02-22 10:07:53 -0300

    Add scaffolding and archives

---
+++ src/component/CardTodos.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CardTodos = () => {
+  return (
+    <div>CardTodos</div>
+  )
+}
+
+export default CardTodos
---
+++ src/component/FormTodo.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FormTodo = () => {
+  return (
+    <div>FormTodo</div>
+  )
+}
+
+export default FormTodo
