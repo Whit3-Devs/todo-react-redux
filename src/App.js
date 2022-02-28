5d0c53be9b3370ba0f3549305dd54c9312f7b46b
Bruno Fernandez <brunofnz28@gmail.com>
2022-02-28 17:28:25 -0300

    Add NextUI and Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import React from "react";
 import { Route } from "react-router-dom";
-// Import NextUI
+import 'bootstrap/dist/css/bootstrap.min.css';
+//Imports NextUI
+import { 
+  NextUIProvider,
+  useTheme, 
+  Container, 
+  Row, 
+  Text
+} from '@nextui-org/react';
+import {  } from '@nextui-org/react';
+
 
 
 // Imports Components
@@ -11,14 +21,19 @@ import Home from "./views/Home";
 
 const App = () => {
 
-  return (
-    <>
+  const { theme } = useTheme();
 
-      <Navbar />
-      <Route exact path="/" component={Home} />
-      <Route exact path="/description" component={Description} />
-      <Route exact path="/about" component={About} />
-    </>
+  return (
+    <NextUIProvider>
+        <Row justify="center" align="center">
+          <Navbar />
+        </Row>
+      <Container xl>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/description" component={Description} />
+          <Route exact path="/about" component={About} />
+      </Container>
+    </NextUIProvider>
   );
 };
 
