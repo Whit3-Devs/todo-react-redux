82ae7d1aa1ab5a208cb7776a908c0d3924fd4d83
Bruno Fernandez <brunofnz28@gmail.com>
2022-02-28 17:31:11 -0300

    Styles need to be added

--- src/component/CardTodos.js
+++ src/component/CardTodos.js
@@ -1,9 +1,34 @@
+import { Container, Row, Col, Spacer} from '@nextui-org/react'
 import React from 'react'
+import TaskCard from './TaskCard'
 
-const CardTodos = () => {
-  return (
-    <div>CardTodos</div>
-  )
+const CardTodos = ({ todos }) => {
+	
+
+	return (
+		<Container>
+			{
+				todos && todos.map((todo) => {
+					return (
+						<>
+							<Row >
+								<TaskCard
+									key={todo.id}
+									title={todo.title}
+									description={todo.description}
+									state={todo.state}
+								/>
+							</Row>
+							<Row>
+								<Spacer y={1}/>
+							</Row>
+						</>
+					)
+				})
+			}
+		</Container>
+
+	)
 }
 
 export default CardTodos
--- src/component/TaskCard.js
+++ src/component/TaskCard.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Container, Row, Col, Card } from '@nextui-org/react';
 
-const TaskCard = () => {
+const TaskCard = ({ title, description, state }) => {
+  const [statee, setStatee] = useState(state)
+
+  const handleState = () => {
+    setStatee(!statee)
+  }
+
   return (
-    <div>TaskCard</div>
+    <Card onClick={handleState} color={statee ? 'success' : 'warning'} css={{ color: 'black' }}>
+            <Row justify="start" align="center">
+                <h2>{title}</h2>
+            </Row>
+            <Row>
+                <Col>
+                    <p>{description}</p>
+                </Col>
+            </Row>
+        </Card>
   )
 }
 
