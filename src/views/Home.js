b97bfcbe92edad854f7bb2ec953a0c68bd955ab7
Bruno Fernandez <brunofnz28@gmail.com>
2022-02-28 17:29:27 -0300

    Add todos for test with CardTodos

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,13 +1,48 @@
+import { Col, Container, Row } from '@nextui-org/react';
 import React from 'react';
 import CardTodos from '../component/CardTodos';
-import FormTodo from '../component/FormTodo'
+import FormTodo from '../component/FormTodo';
+import { Card, Text, Spacer } from '@nextui-org/react';
+
+const todos = [
+  {
+    id: 1,
+    title: 'Comprar tomates',
+    description: 'En lo de doña emma',
+    state: true
+  },
+  {
+    id: 2,
+    title: 'Comprar una cocacola',
+    description: 'En lo de doña emma',
+    state: false
+  },
+  {
+    id: 3,
+    title: 'Lavar la ropa',
+    description: 'Comprar jabon en polvo y tambien vivere perfume',
+    state: false
+  },
+  {
+    id: 4,
+    title: 'Adoptar un perro',
+    description: 'Ver lugares',
+    state: true
+  },
+]
 
 const Home = () => {
   return (
-    <div>
-        <FormTodo />
-        <CardTodos />
-    </div>
+    <Container>
+      <Row>
+        <Col>
+          <Spacer y={2} />
+          <FormTodo />
+          <Spacer y={2} />
+          <CardTodos todos={todos}/>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
